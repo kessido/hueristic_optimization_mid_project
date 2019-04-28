b3ff5c2e053304d73e25a9d1e4912fbc62794ddf
Ido Kessler <kessido@gmail.com>
2019-04-28 11:52:36 +0300

    fixed memory likage

--- last_mile_optimization/abstract_solver.h
+++ last_mile_optimization/abstract_solver.h
@@ -8,17 +8,18 @@ namespace solvers {
 	template<int N>
 	class solver {
 	protected:
-		const problem<N> * prob;
+		const problem<N>* prob;
 		solution<N>* best_solution = nullptr;
 		bool update_best_solution(const solution<N>& s) {
 			if (best_solution == nullptr || best_solution->get_score() > s.get_score()) {
+				if (best_solution != nullptr) delete best_solution;
 				best_solution = new solution<N>(s);
 				return true;
 			}
 			return false;
 		}
 	public:
-		solver(const problem<N>* prob) : prob(prob) {}
+		solver(const problem<N> * prob) : prob(prob) {}
 		~solver() { delete best_solution; }
 		const solution<N>& get_best_solution() const {
 			assert(best_solution != nullptr);
