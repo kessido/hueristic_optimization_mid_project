58c94b213f4e3788470422890f681e788aedf534
Ido Kessler <kessido@gmail.com>
2019-04-30 20:26:22 +0300

    added another statistic function

--- last_mile_optimization/stat.py
+++ last_mile_optimization/stat.py
@@ -18,7 +18,7 @@ def generation_size_stats():
         cur = 0
         print(i)
         for j in range(10):
-            duration,best_score,number_of_solution_tried,number_of_generations = run_with_parameters(bf=2,r=0.2,gsize=int(i))
+            duration,best_score,number_of_solution_tried,number_of_generations = run_with_parameters(bf=1,r=0.2,gsize=int(i))
             cur += best_score
         x.append(i)
         y.append(cur / 10)
@@ -35,12 +35,12 @@ def run_time_effect():
     x = []
     y = []
     i = 0.05
-    while i <= 5:
+    while i <= 1:
         cur = 0
         print(i)
-        rep = 7
+        rep = 10
         for j in range(rep):
-            duration,best_score,number_of_solution_tried,number_of_generations = run_with_parameters(bf=2,r=i,gsize=7)
+            duration,best_score,number_of_solution_tried,number_of_generations = run_with_parameters(bf=1,r=i,gsize=50,mod='single')
             cur += best_score
         x.append(i)
         y.append(cur / rep)
@@ -52,6 +52,25 @@ def run_time_effect():
     ax.set_ylabel('mean_best_solution')
     plt.show()
 
-run_time_effect()
+def crp_effect():
+    x = []
+    y = []
+    i = 0.01
+    while i < 1:
+        cur = 0
+        print(i)
+        for j in range(10):
+            duration,best_score,number_of_solution_tried,number_of_generations = run_with_parameters(bf=1,r=0.2,crp=1-i,gsize=30)
+            cur += best_score
+        x.append(1-i)
+        y.append(cur / 10)
+        i*=1.2
+
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+    ax.set_xlabel('car_randomize_precentage')
+    ax.set_ylabel('best_solution_after_1_second')
+    plt.show()
 
 
+crp_effect()
